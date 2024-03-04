767ad3e9cfd9895afc84e27ae59c35d1ed72e9d5
Wil Fady <wrfady@gmail.com>
2024-03-04 14:45:21 -0500

    completes the rails guide for building a blog

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe "Articles", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/articles/index"
+      get "/articles"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
